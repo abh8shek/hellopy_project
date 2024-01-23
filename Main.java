8a0616d4043b161e457dccca6f073912ffe9c5cb
abh8shek <89271183+abh8shek@users.noreply.github.com>
2024-01-23 10:29:41 +0530

    Module Equity. Main.java has been updated. #6

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			File file = new File("lastcommitmsg.txt");
+			Scanner sc = new Scanner(file);
+			String msg = sc.nextLine();
+			System.out.println(msg);
+			sc.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+			e.printStackTrace();
+		}
+	}
+}
